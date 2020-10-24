e868fcd4838b74c486f6bf2a2aa3683c2d665813
sittisak <xxlbaslxx@gmail.com>
2020-10-24 14:50:04 +0700

    fix: typo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    return res.send('Hello!')
+    return res.send('Hello')
 })
 
 app.listen(port, () => {
