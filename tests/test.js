6e76ab8a7d9c01623834659c3f688a54209dfdc8
sittisak <xxlbaslxx@gmail.com>
2020-10-24 15:27:20 +0700

    add test get '/'

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,15 @@
 const assert = require('assert')
+const request =  require('supertest')
+const app = require('../index')
 
 describe('Validate setting the test', () => {
     it('should be ok', () => {
-        assert.ok(false)
+        assert.ok(true)
+    })
+})
+
+describe('GET "/"', () => {
+    it('respond with Hello!', (done) => {
+        request(app).get('/').expect('Hello!', done)
     })
 })
